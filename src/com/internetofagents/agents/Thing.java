574d5d4a6c9036f2d556810f1a301bb19a0399ea
Matheus Faria <matheus.sousa.faria@gmail.com>
2015-11-27 02:47:32 -0200

    Restructuring

--- src/com/internetofagents/agents/Thing.java
+++ src/com/internetofagents/agents/Thing.java
@@ -6,19 +6,102 @@ import com.internetofagents.behaviors.Ability;
 import com.internetofagents.behaviors.ServiceType;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
-public abstract class Thing extends Agent {
+public class Thing extends Agent {
 
 	private static final long serialVersionUID = 6716848269178218937L;
-	private Hashtable<ServiceType, Ability> abilities; 
+	private Hashtable<ServiceType, Ability > abilities;
 	
 	@Override
 	protected void setup() {
-		this.abilities = new Hashtable<ServiceType, Ability>();
+		this.abilities = new Hashtable<ServiceType, Ability >();
+		
+		try{
+			System.out.println( getLocalName() + " setting up");
+
+            // create the agent descrption of itself
+            DFAgentDescription dfd = new DFAgentDescription();
+            dfd.setName( getAID() );
+            DFService.register( this, dfd );
+
+            // Default thing behaviors
+    		
+    		this.addBehaviour(new OfferServiceBehavior());
+    		this.addBehaviour(new PerformActionBehavior());
+        }
+        catch (Exception e) {
+            System.out.println( "OMG! An Exception: " + e );
+            e.printStackTrace();
+        }
+	}
+	
+	private class OfferServiceBehavior extends CyclicBehaviour {
+
+		private static final long serialVersionUID = 2826783037896725544L;
+
+		@Override
+		public void action() {
+			MessageTemplate cfpTemplate = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = this.myAgent.receive(cfpTemplate);
+			
+			if (msg != null) {
+				// Message received
+				ServiceType serviceType = ServiceType.valueOf(msg.getContent());
+				ACLMessage reply = msg.createReply();
+
+				if (hasAbility(serviceType)) {
+					reply.setPerformative(ACLMessage.PROPOSE);
+					
+					// Maybe send the place?
+					// reply.setContent(...);
+				} else {
+					reply.setPerformative(ACLMessage.REFUSE);
+					
+					// reply.setContent(...);
+				}
+				
+				this.myAgent.send(reply);
+			} else {
+				// Keep waiting
+				block();
+			}
+		}
+		
+	}
+	
+	private class PerformActionBehavior extends CyclicBehaviour {
+
+		private static final long serialVersionUID = 2979730890098622216L;
+
+		@Override
+		public void action() {
+			MessageTemplate cfpTemplate = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = this.myAgent.receive(cfpTemplate);
+			
+			if (msg != null) {
+				// Message received
+				ServiceType serviceType = ServiceType.valueOf(msg.getContent());
+				ACLMessage reply = msg.createReply();
+
+				if (performAction(serviceType)) {
+					reply.setPerformative(ACLMessage.INFORM);
+				} else {
+					reply.setPerformative(ACLMessage.FAILURE);
+				}
+				
+				this.myAgent.send(reply);
+			} else {
+				// Keep waiting
+				block();
+			}
+		}
 	}
 
 	
@@ -41,6 +124,7 @@ public abstract class Thing extends Agent {
 		}
 	}
 	
+	
 	public boolean hasAbility(ServiceType serviceType) {
 		return this.abilities.containsKey(serviceType);
 	}
@@ -49,4 +133,5 @@ public abstract class Thing extends Agent {
 		Ability ability = this.abilities.get(serviceType);
 		return ability.perform();
 	}
+	
 }
