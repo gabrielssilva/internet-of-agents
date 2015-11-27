8943686cf69ed516ff48e13742bc50e14931b963
Matheus Faria <matheus.sousa.faria@gmail.com>
2015-11-27 02:48:05 -0200

    Example Stubs

---
+++ src/com/internetofagents/example/LivingRoomLights.java
@@ -0,0 +1,9 @@
+package com.internetofagents.example;
+
+import com.internetofagents.agents.Thing;
+
+public class LivingRoomLights extends Thing {
+
+	private static final long serialVersionUID = -9179195065594841257L;
+
+}
---
+++ src/com/internetofagents/example/TV.java
@@ -0,0 +1,9 @@
+package com.internetofagents.example;
+
+import com.internetofagents.agents.Thing;
+
+public class TV extends Thing {
+
+	private static final long serialVersionUID = 6470040213650252659L;
+
+}
