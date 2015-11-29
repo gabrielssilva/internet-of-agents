c36b0d493aa810c7131cdf31c42d490bfe1debea
Gabriel Silva <gabriel93.silva@gmail.com>
2015-11-29 17:53:35 -0200

    Adding sniffer filter to the MessagingService

--- src/com/internetofagents/agents/OurMessagingService.java
+++ src/com/internetofagents/agents/OurMessagingService.java
@@ -1,33 +1,54 @@
 package com.internetofagents.agents;
 
+import jade.core.AID;
 import jade.core.BaseService;
+import jade.core.Filter;
 import jade.core.Profile;
 import jade.core.ServiceException;
-import jade.core.messaging.MessagingService;
+import jade.core.VerticalCommand;
+import jade.core.messaging.GenericMessage;
+import jade.core.messaging.MessagingSlice;
+import jade.lang.acl.ACLMessage;
 
-public class OurMessagingService extends MessagingService {
+public class OurMessagingService extends BaseService {
 	
-	/*
 	// Service name
 	public static final String NAME =
 	"com.internetofagents.agents.OurMessagingService";
 	
-	// Service parameter names
-	public static final String VERBOSE =
-	"com_internetofagents_agents_OurMessagingService";
-	
-	private boolean verbose = false;
-*/
 	
 	 public void boot(Profile p) throws ServiceException {
 		 super.boot(p);
-		 //verbose = p.getBooleanProperty(VERBOSE, false);
-		 System.out.println("VERBOSE = ");
+		 System.out.println("Booting the Sniffer service...");
 	 }
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
 	
-	 @Override
-	 public String getName() {
-		 // TODO Auto-generated method stub
-		 return NAME;
-	 }
+	@Override
+	public Filter getCommandFilter(boolean direction) {
+		System.out.println("Adding filters...");
+		return new LoggingFilter();
+	}
+	
+	
+	private class LoggingFilter extends Filter {
+		public boolean accept(VerticalCommand command) {
+			if (command.getName().equals(MessagingSlice.SEND_MESSAGE)) {
+				AID sender = (AID) command.getParam(0);
+				ACLMessage msg = ((GenericMessage) command.getParam(1)).getACLMessage();
+				AID receiver = (AID) command.getParam(2);
+				
+				System.out.println("Messagem intercepted: - - - - - - - - - - - - - - - -");
+				System.out.println("From: "+sender.getName());
+				System.out.println("To: "+receiver.getName());
+				System.out.println("Content: "+msg.getContent());
+				System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - -");
+			}
+			
+			return true;
+		}
+	}
 }
